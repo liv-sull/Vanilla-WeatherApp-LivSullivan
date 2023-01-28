17de10e1d1e38dcc2688151da7c304d446602573
liv-sull <119932394+liv-sull@users.noreply.github.com>
2023-01-28 15:04:38 +1100

    Updated 2nd round Javascript
    Added current time and date stamp

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatData(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -6,12 +29,14 @@ function displayTemperature(response) {
   let feelsLikeElement = document.querySelector("#feelsLike");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#speed");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   feelsLikeElement.innerHTML = Math.round(response.data.temperature.feels_like);
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatData(response.data.time * 1000);
 }
 
 let apiKey = "b6c8908330aoce0f900f83d2e4f0ta35";
